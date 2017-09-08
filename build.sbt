05274369493179a3610bd02ec2b82a50730237c8
Jeffrey Ellin <jellin@qubole.com>
2017-09-08 13:58:48 -0400

    fix build for qubole

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-streaming" % "2.2.0" % "provided",
   "org.apache.spark" %% "spark-sql" % "2.2.0" % "provided",
    "org.apache.spark" % "spark-streaming-kinesis-asl_2.11" % "2.2.0" % "provided"
+  "org.apache.bahir" %% "spark-streaming-twitter" % "2.1.0"
 
 
 )
--- src/main/scala/example/SBSStreamingReader.scala
+++ src/main/scala/example/SBSStreamingReader.scala
@@ -3,12 +3,12 @@ package example
 import com.amazonaws.regions.RegionUtils
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
 import org.apache.spark._
-import org.apache.spark.sql.{SQLContext, SaveMode}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.storage._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kinesis._
-import org.json4s.jackson.JsonMethods.parse;
-import org.json4s.{DefaultFormats}
 /**
   * Created by jellin on 8/18/17.
   */
@@ -23,12 +23,13 @@ object SBSStreamingReader {
     ssc.start()
     ssc.awaitTermination;
 
+
   }
 
 
-  val awsAccessKeyId = ""
-  val awsSecretKey = ""
-  val kinesisStreamName = "sbs-data"
+  val awsAccessKeyId = "AKIAIGY5ZRLFHS3DCGBQ"
+  val awsSecretKey = "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"
+  val kinesisStreamName = "iot-stream"
   val kinesisEndpointUrl = "kinesis.us-east-1.amazonaws.com"
   val kinesisAppName = "SBSStreamingReader"
   val kinesisCheckpointIntervalSeconds = 1
@@ -40,6 +41,7 @@ object SBSStreamingReader {
 
   def creatingFunc(): StreamingContext = {
 
+
     val sparkConf = new SparkConf().setAppName("SBSStreamingReader")
 
     // Create a StreamingContext
@@ -47,37 +49,32 @@ object SBSStreamingReader {
 
 
     // Creata a Kinesis stream
-    val kinesisStream = KinesisUtils.createStream(ssc,
-      kinesisAppName, kinesisStreamName,
-      kinesisEndpointUrl, RegionUtils.getRegionMetadata.getRegionByEndpoint(kinesisEndpointUrl).getName(),
-      InitialPositionInStream.LATEST, Seconds(kinesisCheckpointIntervalSeconds),
-      StorageLevel.MEMORY_AND_DISK_SER_2, awsAccessKeyId, awsSecretKey)
-
-    // Convert the byte array to a string
-    val iot = kinesisStream.map { byteArray => new String(byteArray)}
-
-
-    // Create output csv file at every batch interval
-    iot.foreachRDD { rdd =>
-
-      val sqlContext = new SQLContext(SparkContext.getOrCreate())
-
-      import sqlContext.implicits._
-
-      val jobs = rdd.map(jstr => {
-
-        implicit val formats = DefaultFormats
-
-        val parsedJson = parse(jstr)
-        val j = parsedJson.extract[Beer]
-        j
-      })
-
-      //output the rdd to csv
-      jobs.toDF()
-        .write.mode(SaveMode.Append).csv("/tmp/data/csv")
-
+    val kinesisStream =  (0 until 1).map { i =>
+      KinesisUtils.createStream(ssc,
+        kinesisAppName, kinesisStreamName,
+        kinesisEndpointUrl, RegionUtils.getRegionMetadata.getRegionByEndpoint(kinesisEndpointUrl).getName(),
+        InitialPositionInStream.LATEST, Seconds(kinesisCheckpointIntervalSeconds),
+        StorageLevel.MEMORY_AND_DISK_SER_2, awsAccessKeyId, awsSecretKey)
     }
+
+    val unionStreams =ssc.union(kinesisStream)
+    //Schema of the incoming data on Kinesis Stream
+    val schemaString = "deviceParameter,deviceValue,deviceId,dataTime"
+
+    //Parse the data
+    val tableSchema = StructType( schemaString.split(",").map(fieldName => StructField(fieldName, StringType, true)))
+
+
+    val sqlContext = new SQLContext(SparkContext.getOrCreate())
+    //import sqlContext.implicits._
+
+
+    //Processing each RDD and storing it in temporary table
+    unionStreams.foreachRDD ((rdd: RDD[Array[Byte]], time: Time) => {
+      val rowRDD = rdd.map(jstr => new String(jstr))
+      val df = sqlContext.read.json(rowRDD)
+      df.createOrReplaceTempView("realTimeTable")
+      })
 
     println("Creating function called to create new StreamingContext")
     ssc
